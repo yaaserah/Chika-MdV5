8dd0865c16d9c4f7850ba1e3aafb6bdac2c0d5b7
Yaa <100360558+yaaserah@users.noreply.github.com>
2022-04-25 12:50:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -136,9 +136,9 @@ async function startchika() {
                 }
 
                 if (anu.action == 'add') {
-                    chika.sendMessage(anu.id, { image: { url: ppuser }, contextInfo: { mentionedJid: [num] }, caption: `Welcome To ${metadata.subject} @${num.split("@")[0]}` })
+                    chika.sendMessage(anu.id, { image: { url: ppuser }, contextInfo: { mentionedJid: [num] }, caption: `Welcome To ${metadata.subject} @$ \n\nHarap Baca Deskripsi Grub Sebelum Bertindak{num.split("@")[0]}` })
                 } else if (anu.action == 'remove') {
-                    chika.sendMessage(anu.id, { image: { url: ppuser }, contextInfo: { mentionedJid: [num] }, caption: `@${num.split("@")[0]} Leaving To ${metadata.subject}` })
+                    chika.sendMessage(anu.id, { image: { url: ppuser }, contextInfo: { mentionedJid: [num] }, caption: `@${num.split("@")[0]} TELAH KELUAR SEMOGA SI KONTOL GA BALIK ${metadata.subject}` })
                 }
             }
         } catch (err) {
